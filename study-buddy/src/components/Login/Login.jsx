715c3852d990f1b8ee8c2aec9cfd729afe91a271
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-02-17 21:00:34 -0800

    added login authentication, need to add signup authentication

--- study-buddy/src/components/Login/Login.jsx
+++ study-buddy/src/components/Login/Login.jsx
@@ -2,6 +2,25 @@ import React, { useState } from "react";
 import "./Login.css";
 import login_image from "../../assets/login_image.jpg";
 import logo from "../../assets/logo.png";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.js"; 
+import { useNavigate } from "react-router-dom";
+
+
+const userLogin = async (email, password) => {
+  const auth = getAuth();
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    localStorage.setItem("user", JSON.stringify(userCredential.user));
+    return userCredential.user;
+  }
+  catch (error) {
+    console.error(error);
+    throw error;
+  }
+
+}
+
 
 function LoginPage() {
   const [isLogin, setIsLogin] = useState(true); 
@@ -31,13 +50,31 @@ function LoginPage() {
 }
 
 function LoginForm({ toggleForm }) {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await userLogin(email, password);
+      navigate("/");
+
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("Login failed. Please check your credentials.");
+    }
+  }
+
   return (
     <div id="login-form" className="auth-items-container">
       <img src={logo} className="login-logo" alt="Logo" />
       <h1 style={{textAlign: "center"}} >Welcome to Study Buddy!</h1>
-      <form id="login-form-element" className="auth-form">
-        <input type="email" id="login-email" placeholder="Email" required />
-        <input type="password" id="login-password" placeholder="Password" required />
+      <form id="login-form-element" className="auth-form" onSubmit={handleLogin}>
+        <input type="email" id="login-email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" id="login-password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
 
         <div className="buttons-container">
           <button type="submit" className="action-button">Login</button>
@@ -80,3 +117,4 @@ function SignupForm({ toggleForm }) {
 }
 
 export default LoginPage;
+

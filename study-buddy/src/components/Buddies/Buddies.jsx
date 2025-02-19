31bb01b2820582f9a39154495ff381ed68d742ef
Aliyah Montoya <aliyahmontoya1@gmail.com>
2025-02-18 16:30:08 -0800

    Added the logo & Created a css sheet for layout

--- study-buddy/src/components/Buddies/Buddies.jsx
+++ study-buddy/src/components/Buddies/Buddies.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./Buddies.css"; //Importing the Style Sheet for Buddies
+import logo from "./Logo.png"
 
 const Buddies = () => {
   return (
-    <div>
-      <h2>Welcome to the Buddies Page! uhuhsjnfjs</h2>
+    <div className ="buddies-container">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo-container">
+          <img src={logo} alt ="Logo" className="logo" />
+          
+        </div>
+
+      </nav>
+      <h2>Welcome to the Buddies Page!</h2>
     </div>
   );
 };

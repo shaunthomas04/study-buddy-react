4d919373ec4a963a8b68e095f2b12b2e4e1e0728
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-02-22 22:43:13 -0800

    added study session components to individual day component, added dummy json for genereating tasks to add to day

--- study-buddy/src/components/CalendarDay/CalendarDay.jsx
+++ study-buddy/src/components/CalendarDay/CalendarDay.jsx
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './CalendarDay.css';
 
-const CalendarDay = ({date}) => {
+const CalendarDayDate = ({studySession}) => {
+    console.log(studySession.status);
+    const color = studySession.status === "accepted" ? "green" : studySession.status === "in progress" ? "yellow" : "red";
+    return(
+        <div className="CalendarDay-study-session-outer-container" style={{backgroundColor: color}}> 
+            <div className="CalendarDay-study-session-text"> {studySession.time} with {studySession.person}</div>
+        </div>
+    )
+
+};
+
+
+const CalendarDay = ({date, studySessionsForCalendarDay}) => {
     return(
         <div className="CalendarDay-outer-container">
             <h1 className="CalendarDay-date">{date}</h1>
-            <h2>Calendar Day</h2>
+            <div className='CalendarDay-tasks-container'>
+            {studySessionsForCalendarDay.map((studySession, index) => (
+                    <CalendarDayDate key={index} studySession={studySession} />
+                ))}
+            </div>
 
         </div>
     )

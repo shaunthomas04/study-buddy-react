28c3dbf169d5e6adec4402d0c9ab63dcfb51aa85
Aliyah Montoya <aliyahmontoya1@gmail.com>
2025-03-03 16:50:19 -0800

    added the basics for the page

--- study-buddy/src/components/Buddies/StudyPreferences.jsx
+++ study-buddy/src/components/Buddies/StudyPreferences.jsx
@@ -25,7 +25,43 @@ return (
                     <li><Link to = "/buddies"> Find Study Buddies</Link><li>
                 </ul>
             </aside>
-        </div>
 
-    </>
-)
+            {/* Main Content */}
+            <main className = "study-preferences-content">
+                <div className = "preferences-grid">
+                    <div className = "preferences-card">
+                        <h4>Preferred Study Time</h4>
+                        <p>{preferences.studyTime}</p>
+                    </div>
+
+                    <div className = "preference-card">
+                        <h4>Study Enviroment</h4>
+                        <p>{preferences.enviroment}</p>
+                    </div>
+
+                    <div className = "preference-card">
+                        <h4>Collaboration Style</h4>
+                        <p>{preferences.collaboration}</p>
+                    </div>
+
+                    <div className = "preference-card">
+                        <h4>Type of Learner</h4>
+                        <p>{preferences.typeLearner}</p>
+                    </div>
+                    <div className="preference-card">
+             <h4>Favorite Courses</h4>
+             <ul>
+               {preferences.preferredSubjects.map((courses, index) => (
+                 <li key={index}>{courses}</li>
+               ))}
+             </ul>
+           </div>
+         </div>
+       </main>
+     </div>
+   </>
+ );
+};
+
+export default StudyPreferences;
+

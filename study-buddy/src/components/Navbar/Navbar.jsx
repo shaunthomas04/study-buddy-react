b02f819d3e96b582b7e3082e037f859a457d535d
Joshua Rivera <joshua.rivera1@calbaptist.edu>
2025-03-04 11:15:35 -0800

    place holder profile takes it to agenda page

--- study-buddy/src/components/Navbar/Navbar.jsx
+++ study-buddy/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './Navbar.css';  // Ensure the CSS file is correctly linked
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [searchInput, setSearchInput] = useState("");
 
     return (
         <div className="navbar-container">
             <nav className="navbar">
+                <div className="search-container">
+                    <input 
+                        type="text"
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        className="search-bar"
+                        placeholder="Search..."
+                    />
+                </div>
                 <ul className="nav-list">
                     <li className="nav-item" onClick={() => setActiveIndex(0)}>
                         <Link className={activeIndex === 0 ? "nav-link active" : "nav-link"} to="/">Home</Link>
@@ -20,6 +30,9 @@ const Navbar = () => {
                     </li>
                     <li className="nav-item" onClick={() => setActiveIndex(3)}>
                         <Link className={activeIndex === 3 ? "nav-link active" : "nav-link"} to="/agenda">Agenda</Link>
+                    </li>
+                    <li className="nav-item" onClick={() => setActiveIndex(4)}>
+                        <Link className={activeIndex === 3 ? "nav-link active" : "nav-link"} to="/profile">Profile</Link>
                     </li>
                 </ul>
             </nav>

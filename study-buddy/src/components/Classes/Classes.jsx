6f443495264043a1053c9f7dc8d98f2c80caf797
rachel montenegro <rachelzoe.montenegro@calbaptist.edu>
2025-03-04 10:32:27 -0800

    Fixed Classes Sidebar

--- study-buddy/src/components/Classes/Classes.jsx
+++ study-buddy/src/components/Classes/Classes.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import ClassesSidebar from './ClassesSidebar';
 import './Classes.css';
-
 
 const Classes = () => {
   return (
     <>
       <Navbar />
       <div className="classes-container">
-        <aside className="sidebar">
-          <h3>Classes</h3>
-          <ul>
-            <li className="selected">CSC 312</li>
-            <li>CSC 411</li>
-            <li>EGR 302</li>
-            <li>EGR 328</li>
-            <li>EGR 304</li>
-          </ul>
-        </aside>
-
-        <main className="main-content">
+        <ClassesSidebar />
+        <main className="classes-main-content">
           <h2>Welcome to the Classes Page!</h2>
           <h2>Current Classes</h2>
-
-          <div className="class-card">
-            <h3>CSC 312 - Class Discussion</h3>
-            <p>Ask questions, share notes, and discuss topics here!</p>
-          </div>
         </main>
       </div>
     </>

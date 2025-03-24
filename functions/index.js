7db4e71756c5a4bbd6a2686fe959e86e739bc0e7
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-03-24 12:47:53 -0700

    saving examples to github

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {onDocumentUpdated } = require("firebase-functions/v2/firestore");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//Simple HTTP function
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello world function triggered!");
+  response.send("Hello from Firebase HTTP function!");
+});
+
+// Simple db trigger function
+exports.logUserUpdate = onDocumentUpdated("users/{userId}", (event) => {
+  const previousValue = event.data.before.data();
+  const newValue = event.data.after.data();
+  const userId = event.params.userId;
+
+  logger.log("User updated:", userId);
+  logger.log("Previous value:", previousValue);
+  logger.log("New value:", newValue);
+
+  // You can add more complex logic here based on the update.
+  // Example: Check if a specific field changed.
+  if (previousValue.name !== newValue.name) {
+    logger.log(`User ${userId} name changed from ${previousValue.name} to ${newValue.name}`);
+  }
+
+  return null; // Return null or a promise to indicate completion.
+});

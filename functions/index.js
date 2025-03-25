9dbaf9b42bec5748c650d61262f56edd503962f4
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-03-24 19:30:30 -0700

    started building some functions to try out basic recommendations, updated login to add a user id on creation

--- functions/index.js
+++ functions/index.js
@@ -13,18 +13,92 @@ exports.helloWorld = onRequest((request, response) => {
   response.send("Hello from Firebase HTTP function!");
 });
 
+// Setup Firestore connection
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
  
+// Function to get user info from Firestore
 const getUserInfo = async (userId) => {
     const userRef = db.collection("users").doc(userId);
     const docSnapshot = await userRef.get();
     return docSnapshot.exists ? docSnapshot.data() : null;
 }
 
+// Function to get all users from Firestore
+const getAllUsers = async () => {
+    try {
+      const usersRef = db.collection("users");
+      const snapshot = await usersRef.get();
+  
+      if (snapshot.empty) {
+        throw new Error("No users found");
+      }
+  
+      const usersList = [];
+      snapshot.forEach(doc => {
+        usersList.push({ id: doc.id, ...doc.data() }); 
+      });
+  
+      return usersList; // Return the entire users collection as a list
+    } 
+    catch (error) {
+      throw new Error("Error fetching users: " + error.message);
+    }
+  };
+
+// Function to reccomend buddies to a user
+const reccomendBuddies = async (userId) => {
+    // Get user's courses and buddies info
+    const userInfo = await getUserInfo(userId);
+    if (!userInfo) {
+        logger.error(`User ${userId} not found in Firestore.`);
+        return;
+    }
+    const courses = userInfo.courses;
+    const userBuddies = userInfo.buddies;
+
+    // Look for potential buddies based on user's courses and current buddies
+    const potentialBuddies = new Set();
+
+    // Look through buddys' buddies
+    for (let buddy of userBuddies){
+        const buddyInfo = await getUserInfo(buddy);
+        if (!buddyInfo) {
+            continue;
+        }
+
+        // Add buddy's buddies to potential buddies list if they are not already buddies and not the user
+        const buddyBuddies = buddyInfo.buddies;
+        for (let buddyBuddy of buddyBuddies){
+
+            if (!userBuddies.includes(buddyBuddy.id) && buddyBuddy.id !== userId){
+                potentialBuddies.add(buddyBuddy.id);
+            }            
+        }
+    }
+
+    // Add potential buddies who have the same classes
+    const allUsers = await getAllUsers();
+    for (let buddy of allUsers){
+        const buddyCourses = buddy.courses;
+        const similarCourses = courses.some(course => buddyCourses.includes(course));
+
+        if (similarCourses){
+            potentialBuddies.add(buddy.id);
+        }
 
 
+    }
+
+
+    return Array.from(potentialBuddies);
+}
+
+
+
+
+// Test function to build algorithm until it works then move it into the updateUserOnFirestoreChange function
 exports.getUserInfoHTTP = onRequest(async (req, res) => {
 try {
     const userHash = "L6OWogOvbBV5ywI9B9JgMcRPOSx1";
@@ -36,18 +110,15 @@ try {
         return;
     }
 
-
-    console.log("User data:", userData);
-
-    res.status(200).json(userData);
+    const reccomendations = await reccomendBuddies(userHash);
+    
+    res.status(200).json(reccomendations);
 } 
 catch (error) {
     console.error("Error fetching user:", error);
     res.status(500).json({ error: "Error fetching user: " + error.message });
 }
 });
-
-
 
 
 

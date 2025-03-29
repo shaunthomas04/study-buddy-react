a2d031733723a474daca6f53ecae90b15dad1caf
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-03-29 14:58:47 -0700

    updated study sessions to include sender and reciever ids, added accpet session functionality

--- study-buddy/src/components/CalendarDay/CalendarDay.jsx
+++ study-buddy/src/components/CalendarDay/CalendarDay.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect  } from 'react';
 import './CalendarDay.css';
+import { auth , db } from "../../firebase.js"; 
+import { setDoc, doc, getDoc, updateDoc, arrayUnion, onSnapshot } from "firebase/firestore"; 
 
 // This component is used to show the study sessions for a specific day in a popup when the user clicks on the day in the calendar
 const CalendarDaySessionPopup = ({ calendarDayStudySessions, isOpen, setIsOpen }) => {    
@@ -34,6 +36,41 @@ const CalendarDaySessionPopup = ({ calendarDayStudySessions, isOpen, setIsOpen }
     );
 };
 
+const acceptStudySession = async (senderID, recieverID, studySessionHash) => {
+    try {
+        const reciever = doc(db, "users", recieverID.trim());
+        const recieverDocSnapshot = await getDoc(reciever);  
+        const sender = doc(db, "users", senderID.trim());
+        const senderDocSnapshot = await getDoc(sender);
+
+        if (!recieverDocSnapshot.exists() || !senderDocSnapshot.exists()) {
+            console.log("Error accepting study session");
+            return null;
+        }
+
+        const recieverStudySessions = recieverDocSnapshot.data().agendaStudySessions;
+        const senderStudySessions = senderDocSnapshot.data().agendaStudySessions;
+
+        const updatedReceiverSessions = recieverStudySessions.map(session =>
+            session.sessionID === studySessionHash ? { ...session, status: "accepted" } : session
+        );
+        const updatedSenderSessions = senderStudySessions.map(session =>
+            session.sessionID === studySessionHash ? { ...session, status: "accepted" } : session
+        );
+
+        await updateDoc(reciever, { agendaStudySessions: updatedReceiverSessions });
+        await updateDoc(sender, { agendaStudySessions: updatedSenderSessions });
+
+      } 
+      catch (error) {
+        console.error("Error getting document:", error);
+        return null;
+      }
+  }
+
+
+
+
 //This component is used to show the study sessions for a day in a popup when the pipup is open
 const CalendarPopupSessions = ({studySession}) => {
     const green = "#71FF65";
@@ -41,16 +78,20 @@ const CalendarPopupSessions = ({studySession}) => {
     const blue = "#6E6FFF";
     const red = "#FF5B57";
 
-  const color = studySession.status === "accepted" ? green : studySession.status === "pending" ? yellow : studySession.status === "request" ? blue: red;
-  const studySessionText = studySession.status === "accepted" ? `Study session with ${studySession.person} at ${studySession.time}`
-   : studySession.status === "pending" ? `Study session with ${studySession.person} at ${studySession.time} is pending` 
-   : studySession.status === "request" ? `Study session with ${studySession.person} at ${studySession.time} is requested`
-   : `Study session with ${studySession.person} at ${studySession.time} has been rejected`;
+    const color = studySession.status === "accepted" ? green : studySession.status === "pending" ? yellow : studySession.status === "request" ? blue: red;
+    const studySessionText = studySession.status === "accepted" ? `Study session with ${studySession.person} at ${studySession.time}`
+    : studySession.status === "pending" ? `Study session with ${studySession.person} at ${studySession.time} is pending` 
+    : studySession.status === "request" ? `Study session with ${studySession.person} at ${studySession.time} is requested`
+    : `Study session with ${studySession.person} at ${studySession.time} has been rejected`;
 
-   const [isOpen, setIsOpen] = useState(false);
-   const togglePopup = () => {
-    setIsOpen(prevState => !prevState); 
-};
+    const [isOpen, setIsOpen] = useState(false);
+    const togglePopup = () => {
+            setIsOpen(prevState => !prevState); 
+        };
+
+    const senderID = studySession.senderID;
+    const receiverID = studySession.recieverID;
+    const studySessionID = studySession.sessionID;
 
 
     return(
@@ -59,7 +100,15 @@ const CalendarPopupSessions = ({studySession}) => {
 
             {isOpen && (
                 <div className="CalendarDay-popup-session-notes">
-                {studySession.notes}
+                    {studySession.notes}
+
+                    {studySession.status === "request" && (
+                        <div>
+                            <button onClick={async () => await acceptStudySession(senderID, receiverID, studySessionID)} className="CalendarDay-popup-session-buttons" style={{"backgroundColor": "green"}}>✓</button>
+                            <button className="CalendarDay-popup-session-buttons" style={{"backgroundColor": "red"}}>x</button>
+                        </div>
+                    )}
+
                 </div>
             )}
         </div>

dc92f88c47e8a98be2d97396477945c0d9192316
rachel montenegro <rachelzoe.montenegro@calbaptist.edu>
2025-04-09 20:51:16 -0700

    fixing position of logo on loading page

--- study-buddy/src/components/Redirect/Redirect.jsx
+++ study-buddy/src/components/Redirect/Redirect.jsx
@@ -6,10 +6,13 @@ const Redirect = () => {
     return (
         <>
         <Navbar />
-        <div className="loading-content">
-            <h1>Add classes in profile page</h1>
-            <div className="loader"></div>
-        </div>
+            <div className="redirect-container">
+                <img
+                    className="logo"
+                    src="https://cdn.discordapp.com/attachments/1343710392693686283/1359241235302449192/Adobe_Express_-_file_2.png?ex=67f6c38b&is=67f5720b&hm=73dfe69884f8ad35c0f36d8526645b279ce4e826f9418a10f6a3cb2c6815411d&format=webp&quality=lossless&width=891&height=889"
+                    alt="Logo"
+                />
+            </div>
         </>
     );
 }
